aec2f3d371c50d13a7173dc406c9d6ddcffab2ce
Victor Hugo <pavictorhugo@gmail.com>
2020-08-18 16:09:05 -0300

    Create CRD Repository

--- src/App.js
+++ src/App.js
@@ -1,31 +1,62 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+    useEffect(() => {
+      api.get('repositories').then(response => {
+        setRepositories(response.data);
+      })
+    },[]);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: 'React Native',
+      url: 'http://github.com/thorugoh',
+      techs: ['a', 'b']
+    })
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const {status} = await api.delete(`repositories/${id}`);
+
+    if(status == 204)
+    {
+      const filteredRepositories = repositories.filter(repository => repository.id != id)
+      setRepositories(filteredRepositories);
+    }
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+      <div>
+        <ul data-testid="repository-list">
+          
+          {repositories.map(repository => {
+              return (
+                <li key={repository.id}>
+                  {repository.title}
+                  <button onClick={() => handleRemoveRepository(repository.id)}>
+                    Remover
+                  </button>
+                </li>
+              )
+          })}
+          
+        </ul>
+  
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </div>
+    );
+  }
+  
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
-}
 
 export default App;
